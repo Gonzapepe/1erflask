98942b7907498be51b86b790d46865ea9db11955
Gonzapepe <addamogonzalo@gmail.com>
2019-11-08 21:07:46 -0300

    página base terminada

--- app.py
+++ app.py
@@ -35,12 +35,38 @@ def add_contact():
         mysql.connection.commit()
         flash('Contacto agregado satisfactoriamente.')
         return redirect(url_for('Index'))
-@app.route('/edit')
-def edit_contact():
-    return 'edit contact'
-@app.route('/delete')
-def delete_contact():
-    return 'delete contact'
+@app.route('/edit/<id>')
+def get_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts WHERE id = %s', (id))
+    data = cur.fetchall()
+    print(data)
+    return render_template('edit-contact.html', contact = data[0])
+
+@app.route('/update/<id>', methods = ['POST'])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE contacts
+            SET fullname = %s,
+            email = %s,
+            phone = %s
+            WHERE id = %s
+        """, (fullname, email, phone, id))
+        mysql.connection.commit()
+        flash('Contacto actualizado satisfactoriamente.')
+        return redirect(url_for('Index'))
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Contacto removido satisfactoriamente.')
+    return redirect(url_for('Index'))
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
